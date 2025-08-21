15e127bb5dc5cf9c279219cb7211b2becf0b443d
Carlos Menjivar <menjivarc@gmail.com>
2025-08-21 23:12:40 +0000

    adding infrastructure

---
+++ main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_version = ">=1.3.7"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.43.0"
+    }
+  }
+  cloud {
+    organization = "CARLOS-training"
+    workspaces {
+      name = "remotestate"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}                       #empty features block
+  skip_provider_registration = true #environment with limited permissions
+}
+
+# Generates a random suffix
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "688-a8481db2-menjivar-test-5m6"
+  location = "West US"
+}
+
+# Register Azure subscription to use the Microsoft.Storage resource provider
+resource "azurerm_resource_provider_registration" "storage" {
+  name = "Microsoft.Storage"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "sttest${random_id.suffix.hex}"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "RAGRS"
+
+  # Update your storage account resource to depend on the registration
+  depends_on = [
+    azurerm_resource_provider_registration.storage
+  ]
+}
