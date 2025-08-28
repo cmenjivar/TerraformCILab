ba53c4dd944d0bd8a715cb9cfad4909cbbd64254
Carlos Menjivar <menjivarc@gmail.com>
2025-08-28 23:53:42 +0000

    storage account name update

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "azurerm_resource_provider_registration" "storage" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "sttest${random_id.suffix.hex}"
+  name                     = "stmenjivartest${random_id.suffix.hex}"
   location                 = azurerm_resource_group.rg.location
   resource_group_name      = azurerm_resource_group.rg.name
   account_tier             = "Standard"
